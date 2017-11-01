b86ae04f3b531f730e6f4bc09c4b8cc4e624d0b2
Glitch (reminiscent-guide) <none>
2017-11-01 15:34:31 +0000

    Exporting to Lycrios/FCC-Timestamp-Microservice

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var app = express();
+const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
+const express = require('express');
+const app = express();
 app.use(express.static('public'));
 
 function isNumeric(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function isValidDate(s) {
+  var result = new Date(s);
+  if(result == 'Invalid Date'){
+    return false;
+  }
+  return true;
 }
 
 app.get("/:data", function (req, res) {
@@ -11,7 +21,16 @@ app.get("/:data", function (req, res) {
   var obj = {};
   
   if(isNumeric(params)){
-    
+    var dt = new Date(parseInt(params)*1000);
+    obj.unix = dt.getTime() / 1000;
+    obj.natural = months[dt.getMonth()]+" "+dt.getDate()+", "+dt.getFullYear();
+  }else if(isValidDate(params)){
+    var dt = new Date(params);
+    obj.unix = dt.getTime() / 1000;
+    obj.natural = months[dt.getMonth()]+" "+dt.getDate()+", "+dt.getFullYear();
+  }else{
+    obj.unix = null;
+    obj.natural = null;
   }
   
   res.set({'content-type': 'application/json'});
